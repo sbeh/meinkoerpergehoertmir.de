d7dd8cb9909de2a41c4bdea02bfd953a93266822
Cnallrola <carolabehrens1@gmail.com>
2017-12-08 16:04:31 +0100

    php zu html

--- js/replace.js
+++ js/replace.js
@@ -4,9 +4,13 @@ function replaceElem(elem)
 {
     var x = new XMLHttpRequest()
 	x.onreadystatechange = function () {
-		if (x.readyState === XMLHttpRequest.DONE && x.status === 200) {
-            elem.outerHTML = x.response
-            handleReplace()
+		if (x.readyState === XMLHttpRequest.DONE) {
+            if (x.status === 200) {
+                elem.outerHTML = x.response
+                handleReplace()
+            } else {
+                elem.outerHTML = 'Fehler! Konnte ' + elem.textContent + ' nicht ersetzen.<br>'
+            }
         }
 	}
 	x.open('GET', 'module/' + elem.textContent + '.html')
