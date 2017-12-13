16d15f3bb96602b23f3455308e99ba64149a02f9
Cnallrola <31159009+Cnallrola@users.noreply.github.com>
2017-12-13 22:57:00 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -12,18 +12,19 @@ loadScript('js/jquery.lightbox-0.5.js');
 loadScript('js/replace.js');
 loadScript('js/state_navigation.js');
 
-var sBrowser, sUsrAg = navigator.userAgent;
 
-if(sUsrAg.indexOf("Chrome") > -1) {
-    sBrowser = "Google Chrome";
-} else if (sUsrAg.indexOf("Safari") > -1) {
-    sBrowser = "Apple Safari";
-} else if (sUsrAg.indexOf("Opera") > -1) {
-    sBrowser = "Opera";
-} else if (sUsrAg.indexOf("Firefox") > -1) {
-    sBrowser = "Mozilla Firefox";
-} else if (sUsrAg.indexOf("MSIE") > -1) {
-    sBrowser = "Microsoft Internet Explorer";
+Beispiel #2: Browsererkennung und Rückgabe eines Index.
+
+function getBrowserId () {
+
+    var
+        aKeys = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"],
+        sUsrAg = navigator.userAgent, nIdx = aKeys.length - 1;
+
+    for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
+
+    return nIdx
+
 }
 
-console.log("Sie verwenden: " + sBrowser);
+console.log(getBrowserId());
