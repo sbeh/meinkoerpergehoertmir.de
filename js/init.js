c42456a8964396196ee2122a6b76e7048baa92b0
Cnallrola <31159009+Cnallrola@users.noreply.github.com>
2017-12-13 22:46:59 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -12,6 +12,6 @@ loadScript('js/jquery.lightbox-0.5.js');
 loadScript('js/replace.js');
 loadScript('js/state_navigation.js');
 
-var match = $window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+var match = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
 var ver = match ? parseInt(match[1]) : 0;
 console.log(ver);
