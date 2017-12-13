7e80584a329dc6c37b97d9f11c78c648ad7ebd49
Cnallrola <31159009+Cnallrola@users.noreply.github.com>
2017-12-14 00:29:51 +0100

    Update Browser.js

--- js/Browser.js
+++ js/Browser.js
@@ -15,7 +15,7 @@ function getBrowserId () {
 var kk = getBrowserId();
 if (kk === 1) {
     var meinevariable = document.getElementsByClassName('article__left--item');
-    for(z=0;z<meinevariable.length();z++){
+    for(z=0,z<meinevariable.length(),z++){
         console.log(z);
    // meinevariable.remove('article__left--item');
    // meinevariable.add('useragent__firefox--item');
