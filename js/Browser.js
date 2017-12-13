5fec3830c9f7de202f1c04e8d2ebb4200357d84a
Cnallrola <31159009+Cnallrola@users.noreply.github.com>
2017-12-13 23:25:13 +0100

    Update Browser.js

--- js/Browser.js
+++ js/Browser.js
@@ -8,13 +8,10 @@ function getBrowserId () {
 
     for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
 
-    return nIdx
-
-}
-var intKey = document.addEventListener("DOMContentLoaded", getBrowserId(), true)
-console.log('kk');
-console.log(intKey);
-if (intKey === 1) {
+    if (nIdx === 1) {
     var meinevariable = document.querySelector("article");
     meinevariable.className += "test";
+        
 }
+    
+document.addEventListener("DOMContentLoaded", getBrowserId(), true)
