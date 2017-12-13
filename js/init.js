908b6d6ec088bb5b6e4d451092b1c122ed7a6d41
Cnallrola <31159009+Cnallrola@users.noreply.github.com>
2017-12-13 22:53:59 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -16,5 +16,14 @@ var sBrowser, sUsrAg = navigator.userAgent;
 
 if(sUsrAg.indexOf("Chrome") > -1) {
     sBrowser = "Google Chrome";
-} 
+} else if (sUsrAg.indexOf("Safari") > -1) {
+    sBrowser = "Apple Safari";
+} else if (sUsrAg.indexOf("Opera") > -1) {
+    sBrowser = "Opera";
+} else if (sUsrAg.indexOf("Firefox") > -1) {
+    sBrowser = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("MSIE") > -1) {
+    sBrowser = "Microsoft Internet Explorer";
+}
+
 console.log("Sie verwenden: " + sBrowser);
