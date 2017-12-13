980aee9699af9a0433edf602c2b9bb92021e8299
Cnallrola <31159009+Cnallrola@users.noreply.github.com>
2017-12-13 23:03:47 +0100

    Create Browser.js

---
+++ js/Browser.js
@@ -0,0 +1,11 @@
+function getBrowserId () {
+
+    var
+        aKeys = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"],
+        sUsrAg = navigator.userAgent, nIdx = aKeys.length - 1;
+
+    for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
+
+    return nIdx
+
+}
