f88f807e369cc25963fd5dba01772fd5b6e381e1
Cnallrola <31159009+Cnallrola@users.noreply.github.com>
2017-12-14 09:35:32 +0100

    Update Browser.js

--- js/Browser.js
+++ js/Browser.js
@@ -6,10 +6,14 @@ window.onload = function () {
      var kk = getBrowserId();
  console.log('weooeoe:' + kk);
      if (kk === 1) {
+      if(document.getElementsByTagName('article') !== undefined){
+        console.log('nichts');
+       else{
           console.log('zeile 8:  ' +  document.getElementsByTagName('article'));
              //document.getElementsByTagName('article').classList.remove("articel__left--item");
               // console.log(document.getElementsByTagName('article').size());
              document.getElementsByTagName("article").classList.add("useragent__firefox--item");
+       }
      }
 
 }
